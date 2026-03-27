9f25f7bdf123301b4c38e957e95d37bab56d40e8
Vitao-campos <vaffonsocampos@gmail.com>
2026-03-26 22:22:43 -0300

    adiciona script no json

--- hello.js
+++ hello.js
@@ -4,3 +4,11 @@ console.log("Meu primeiro programa em JS!");
 const nome = "Victor";
 console.log("E aí, " + nome + ", suave?");
 
+const nomes = 'Carlos';
+const idade = 22;
+
+// Concatenação clássica (como em C com sprintf)
+console.log('Nome: ' + nomes + ', Idade: ' + idade);
+
+// Template literal (moderno e mais legível)
+console.log(`Nome: ${nomes}, Idade: ${idade}`);
