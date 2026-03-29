dadc902dcae3b585cbb3d52325d006b63ba71dc3
Vitao-campos <vaffonsocampos@gmail.com>
2026-03-29 12:51:01 -0300

    adiciona perfil ao projeto

---
+++ perfil.js
@@ -0,0 +1,5 @@
+const nome = 'Victor';
+const idade = 31;
+const curso = 'ADS';
+const semestre = 2;
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e estou no ${semestre}º semestre do curso de ${curso} da FSTEC.`);
