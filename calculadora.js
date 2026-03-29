4a720d5c2d6d9ec5207d855bf4a5f9724114a898
Vitao-campos <vaffonsocampos@gmail.com>
2026-03-29 12:43:24 -0300

    adiciona calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,45 @@
+// Valores para a calculadora
+const a = 10;
+const b = 5;
+
+// SOMA
+const soma = a + b;
+console.log(`${a} + ${b} = ${soma}`);
+
+// SUBTRAÇÃO
+const subtracao = a - b;
+console.log(`${a} - ${b} = ${subtracao}`);
+
+// MULTIPLICAÇÃO
+const multiplicacao = a * b;
+console.log(`${a} * ${b} = ${multiplicacao}`);
+
+// DIVISÃO
+const divisao = a / b;
+console.log(`${a} / ${b} = ${divisao}`);
+
+// FUNÇÃO DE CALCULADORA
+//function calcular(num1, num2, operacao) {
+  //switch(operacao) {
+    //case '+':
+      //return num1 + num2;
+    ///case '-':
+      //return num1 - num2;
+    ///case '*':
+      ///return num1 * num2;
+    ///case '/':
+      ///if (num2 === 0) {
+        ///return "Erro: Divisão por zero!";
+      //}
+      ///return num1 / num2;
+    ///default:
+      ///return "Operação inválida";
+  //}
+//}
+
+// Testando a função
+//console.log("\n--- Usando a função ---");
+//console.log(`calcular(20, 4, '+') = ${calcular(20, 4, '+')}`);
+//console.log(`calcular(20, 4, '-') = ${calcular(20, 4, '-')}`);
+//console.log(`calcular(20, 4, '*') = ${calcular(20, 4, '*')}`);
+//console.log(`calcular(20, 4, '/') = ${calcular(20, 4, '/')}`);
