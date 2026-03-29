c4b189b5eb248c75e133c499094e5fda0ad39835
Vitao-campos <vaffonsocampos@gmail.com>
2026-03-29 13:02:46 -0300

    adiciona conversor ao projeto

---
+++ conversor.js
@@ -0,0 +1,8 @@
+const temperatura = 31;
+// fahrenheit = (celsius * 9/5) + 32
+console.log (`a temperatura em celsius é: ${temperatura} °C`);
+const fahrenheit = (temperatura * 9/5) + 32;
+console.log (`a temperatura em fahrenheit é: ${fahrenheit} °F`);
+// kelvin = celsius + 273.15
+const kelvin = temperatura + 273.15;
+console.log (`a temperatura em kelvin é: ${kelvin} K`);
